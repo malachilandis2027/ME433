073d379371ac0320a800f1a93cb48cdd053242e4
malachilandis2027 <124309194+malachilandis2027@users.noreply.github.com>
2023-05-31 14:36:14 -0500

    update code

---
+++ HW8/HW8.X/nbproject/Makefile-default.mk
@@ -0,0 +1,206 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=../hw8_main.c ../i2c_master_noint.c ../mpu6050.c ../nu32dip.c ../ssd1306.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/hw8_main.o ${OBJECTDIR}/_ext/1472/i2c_master_noint.o ${OBJECTDIR}/_ext/1472/mpu6050.o ${OBJECTDIR}/_ext/1472/nu32dip.o ${OBJECTDIR}/_ext/1472/ssd1306.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/hw8_main.o.d ${OBJECTDIR}/_ext/1472/i2c_master_noint.o.d ${OBJECTDIR}/_ext/1472/mpu6050.o.d ${OBJECTDIR}/_ext/1472/nu32dip.o.d ${OBJECTDIR}/_ext/1472/ssd1306.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/_ext/1472/hw8_main.o ${OBJECTDIR}/_ext/1472/i2c_master_noint.o ${OBJECTDIR}/_ext/1472/mpu6050.o ${OBJECTDIR}/_ext/1472/nu32dip.o ${OBJECTDIR}/_ext/1472/ssd1306.o
+
+# Source Files
+SOURCEFILES=../hw8_main.c ../i2c_master_noint.c ../mpu6050.c ../nu32dip.c ../ssd1306.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=32MX170F256B
+MP_LINKER_FILE_OPTION=,--script="..\NU32DIPbootloaded.ld"
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assembleWithPreprocess
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/1472/hw8_main.o: ../hw8_main.c  .generated_files/flags/default/6f855c9b10e5d6cf4ddd8fbee1f6f86ccb819f5d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/hw8_main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/hw8_main.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/hw8_main.o.d" -o ${OBJECTDIR}/_ext/1472/hw8_main.o ../hw8_main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/i2c_master_noint.o: ../i2c_master_noint.c  .generated_files/flags/default/2b618caecd86c5123e3fcf54b6b987f431bd648a .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/i2c_master_noint.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/i2c_master_noint.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/i2c_master_noint.o.d" -o ${OBJECTDIR}/_ext/1472/i2c_master_noint.o ../i2c_master_noint.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/mpu6050.o: ../mpu6050.c  .generated_files/flags/default/375a9fa174f41da768c1fdadb7b4cb6d70296549 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/mpu6050.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/mpu6050.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/mpu6050.o.d" -o ${OBJECTDIR}/_ext/1472/mpu6050.o ../mpu6050.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/nu32dip.o: ../nu32dip.c  .generated_files/flags/default/d5697a805ac303c0f43e739b6d0383460b582154 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/nu32dip.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/nu32dip.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/nu32dip.o.d" -o ${OBJECTDIR}/_ext/1472/nu32dip.o ../nu32dip.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/ssd1306.o: ../ssd1306.c  .generated_files/flags/default/5818095a3ba67dffb2fdd769a6fd9f9569a18fb7 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/ssd1306.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/ssd1306.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/ssd1306.o.d" -o ${OBJECTDIR}/_ext/1472/ssd1306.o ../ssd1306.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+else
+${OBJECTDIR}/_ext/1472/hw8_main.o: ../hw8_main.c  .generated_files/flags/default/1f996405b45b710a928df5c2f5bf916f8f513b80 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/hw8_main.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/hw8_main.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/hw8_main.o.d" -o ${OBJECTDIR}/_ext/1472/hw8_main.o ../hw8_main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/i2c_master_noint.o: ../i2c_master_noint.c  .generated_files/flags/default/6d2ad85950a0ca45dddf50973cb61abd95eb2ff .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/i2c_master_noint.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/i2c_master_noint.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/i2c_master_noint.o.d" -o ${OBJECTDIR}/_ext/1472/i2c_master_noint.o ../i2c_master_noint.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/mpu6050.o: ../mpu6050.c  .generated_files/flags/default/d1b362b5b333122aef15be78ad79b20a17880e98 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/mpu6050.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/mpu6050.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/mpu6050.o.d" -o ${OBJECTDIR}/_ext/1472/mpu6050.o ../mpu6050.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/nu32dip.o: ../nu32dip.c  .generated_files/flags/default/8c1d7eef3f73fda871d6aa69371b7a29cd41350b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/nu32dip.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/nu32dip.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/nu32dip.o.d" -o ${OBJECTDIR}/_ext/1472/nu32dip.o ../nu32dip.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+${OBJECTDIR}/_ext/1472/ssd1306.o: ../ssd1306.c  .generated_files/flags/default/105cf1e5be0dced5daf00bbc4e32b08bb1ea0dd3 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}/_ext/1472" 
+	@${RM} ${OBJECTDIR}/_ext/1472/ssd1306.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1472/ssd1306.o 
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/1472/ssd1306.o.d" -o ${OBJECTDIR}/_ext/1472/ssd1306.o ../ssd1306.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compileCPP
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    ../NU32DIPbootloaded.ld
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g   -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
+	
+else
+${DISTDIR}/HW8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   ../NU32DIPbootloaded.ld
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/HW8.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
+	${MP_CC_DIR}\\xc32-bin2hex ${DISTDIR}/HW8.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
--- HW8/hw8_main.c
+++ HW8/hw8_main.c
@@ -4,22 +4,19 @@
 #include <stdio.h>
 
 void blink(int, int); // blink the LEDs function
+void drawChar(unsigned char, unsigned char, unsigned char); // draw character at x,y location
+void drawString(unsigned char*, unsigned int, unsigned char, unsigned char); // draw string at x,y location
 
 int main(void) {
     NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
     init_mpu6050();
+    ssd1306_setup();
 	
 	// char array for the raw data
     char data[14]; // 16 bits per piece of data, 7 pieces of data (accel x,y,z, gyro x,y,z, and temp)
 	// floats to store the data
-	float accelX;
-    float accelY;
     float accelZ;
-    float gyroX;
-    float gyroY;
-    float gyroZ;
-    float temp;
-    
+
 	// read whoami
     uint8_t who = whoami();
 	// print whoami
@@ -36,28 +33,24 @@ int main(void) {
     char m_in[100];
     NU32DIP_ReadUART1(m_in,100);
 
+    unsigned long time = 1;
     while (1) {
-		// use core timer for exactly 100Hz loop
         _CP0_SET_COUNT(0);
         blink(1, 5);
 
         // read IMU
         burst_read_mpu6050(data);
 		// convert data
-        accelX = conv_xXL(data);
-        accelY = conv_yXL(data);
         accelZ = conv_zXL(data);
-        gyroX = conv_xG(data);
-        gyroY = conv_yG(data);
-        gyroZ = conv_zG(data);
-        temp = conv_temp(data);
         
-        // print out the data
-        sprintf(message,"%f,%f,%f,%f,%f,%f,%f",accelX,accelY,accelZ,gyroX,gyroY,gyroZ,temp);
-        NU32DIP_WriteUART1(message);
+        sprintf(message,"Z Accel: %f",accelZ);
+        drawString(message,16,10,10);
+        sprintf(message,"FPS: %f",1/float(time));
+        drawString(message,12,10,1);
         
-        while (_CP0_GET_COUNT() < 48000000 / 2 / 100) {
-        }
+        // sprintf(message,"FPS: %f",1/time);
+        // NU32DIP_WriteUART1(message);
+        time = _CP0_GET_COUNT();
     }
 }
 
@@ -83,3 +76,24 @@ void blink(int iterations, int time_ms) {
     }
 }
 
+// draw character
+void drawChar(unsigned char letter, unsigned char x, unsigned char y) {
+    unsigned char columnHex; // Char used to store the hex value of a column
+    unsigned char color; // Char used to store the pixel's color (0 or 1)
+    for (int i = 0; i < 5; i++){ // Loop over each element of the list (columns)
+        columnHex = ASCII[letter-0x20][i];
+        for (int j = 7; j > -1; j--) { // Looping over the row
+            color = 0b00000001 & columnHex; // Get the value of the LSB of columnHex
+            ssd1306_drawPixel(x+i,y+j,color); // Draw the pixel
+            columnHex = columnHex>>1; // Right shift by 1 to access the next pixel
+        }
+    }
+}
+
+void drawString(unsigned char* message, unsigned int length, unsigned char x, unsigned char y) {
+    ssd1306_clear();
+    for (int i = 0; i < length; i++) {
+        drawChar(message[i],x+5*i,y);
+    }
+    ssd1306_update();
+}
